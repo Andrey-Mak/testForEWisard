f787ff46f44ce37295adc82b814cf3bd3f75b042
Andrey Makushak <a.makushak@viseven.com>
2019-11-14 19:35:22 +0200

    update(Base)

--- src/views/Base.vue
+++ src/views/Base.vue
@@ -2,48 +2,65 @@
 	<v-data-table
 			:headers="headers"
 			:items="users"
-			sort-by=""
-			class="elevation-1">
+			:search="search"
+			class="elevation-1"
+			loading-text="Loading... Please wait">
+
 		<template v-slot:top>
-			<v-toolbar flat color="white">
-				<v-toolbar-title>Contacts</v-toolbar-title>
-				<v-divider class="mx-4" inset vertical></v-divider>
-				<v-toolbar-title>Favorites</v-toolbar-title>
-				<v-spacer></v-spacer>
-				<v-dialog v-model="dialog" max-width="500px">
-					<template v-slot:activator="{ on }">
-						<v-btn color="primary" dark class="mb-2" v-on="on">NEW CONTACT</v-btn>
-					</template>
-					<v-card>
-						<v-card-title>
-							<span class="headline">{{ formTitle }}</span>
-						</v-card-title>
-						<v-card-text>
-							<v-container>
-								<v-text-field v-model="editedItem.first_name"
-											  label="First name"></v-text-field>
-								<v-text-field v-model="editedItem.last_name"
-											  label="Last name"></v-text-field>
-								<v-text-field v-model="editedItem.phone_number"
-											  label="Phone"></v-text-field>
-							</v-container>
-						</v-card-text>
-						<v-card-actions>
+			<v-container>
+						<v-text-field
+								class="mx-4 md-4"
+								solo
+								v-model="search"
+								label="Search"
+								prepend-inner-icon="mdi-magnify"
+						></v-text-field>
+
+						<v-toolbar color="white">
+							<v-toolbar-title @click="isFavorites = false">Contacts</v-toolbar-title>
+							<v-divider class="mx-4" inset vertical></v-divider>
+							<v-toolbar-title @click="isFavorites = true">Favorites</v-toolbar-title>
 							<v-spacer></v-spacer>
-							<v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-							<v-btn color="blue darken-1" text @click="save">Save</v-btn>
-						</v-card-actions>
-					</v-card>
-				</v-dialog>
-			</v-toolbar>
+							<v-dialog v-model="dialog" max-width="500px">
+								<template v-slot:activator="{ on }">
+									<v-btn color="primary" dark class="mb-2" v-on="on">NEW CONTACT
+									</v-btn>
+								</template>
+								<v-card>
+									<v-card-title>
+										<span class="headline">{{ formTitle }}</span>
+									</v-card-title>
+									<v-card-text>
+										<v-container>
+											<v-text-field v-model="editedItem.first_name"
+														  label="First name"></v-text-field>
+											<v-text-field v-model="editedItem.last_name"
+														  label="Last name"></v-text-field>
+											<v-text-field v-model="editedItem.phone_number"
+														  label="Phone"></v-text-field>
+										</v-container>
+									</v-card-text>
+									<v-card-actions>
+										<v-spacer></v-spacer>
+										<v-btn color="blue darken-1" text @click="close">Cancel
+										</v-btn>
+										<v-btn color="blue darken-1" text @click="save">Save</v-btn>
+									</v-card-actions>
+								</v-card>
+							</v-dialog>
+						</v-toolbar>
+
+			</v-container>
 		</template>
 		<template v-slot:item.action="{ item }">
 			<v-icon small class="mr-2"
 					v-text="item.favorite ? 'mdi-bookmark' : 'mdi-bookmark-outline'"
+					v-model="isFavorites"
 					@click="favoriteUser(item)">favorite
 			</v-icon>
 			<v-icon small class="mr-2" v-text="'mdi-pencil'" @click="editUser(item)">edit</v-icon>
-			<v-icon small class="mr-2" v-text="'mdi-delete'" @click="deleteUser(item)">delete</v-icon>
+			<v-icon small class="mr-2" v-text="'mdi-delete'" @click="deleteUser(item)">delete
+			</v-icon>
 		</template>
 		<template v-slot:no-data>
 			<v-btn color="primary" @click="initialize">Reset</v-btn>
@@ -56,29 +73,39 @@
 
 	export default {
 		name: 'tabs',
-		data: () => ({
-			dialog: false,
-			headers: [
-				{
-					text: 'Name',
-					align: 'left',
-					sortable: false,
-					value: 'first_name'
+		data(){
+			return {
+				dialog: false,
+				headers: [
+					{
+						text: 'Name',
+						align: 'left',
+						sortable: false,
+						value: 'first_name'
+					},
+					{text: 'Phone', value: 'phone_number'},
+					{text: 'Actions', value: 'action', sortable: false,
+						filter: (value, search, item) => {
+							if(!this.isFavorites) return true;
+							return this.isFavorites && item.favorite;
+						}
+					}
+				],
+				search: '',
+				isFavorites: false,
+				loading: true,
+				users: [],
+				editedIndex: -1,
+				editedItem: {
+					first_name: '',
+					phone_number: ''
 				},
-				{text: 'Phone', value: 'phone_number'},
-				{text: 'Actions', value: 'action', sortable: false},
-			],
-			users: [],
-			editedIndex: -1,
-			editedItem: {
-				first_name: '',
-				phone_number: ''
-			},
-			defaultItem: {
-				first_name: '',
-				phone_number: ''
-			},
-		}),
+				defaultItem: {
+					first_name: '',
+					phone_number: ''
+				}
+			}
+		},
 		computed: {
 			formTitle(){
 				return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
@@ -96,7 +123,6 @@
 					.finally(() => (this.loading = false));
 			},
 			favoriteUser(user){
-				console.log(user);
 				user.favorite = !user.favorite;
 				this.$store.dispatch('editUser', user);
 			},
@@ -116,9 +142,9 @@
 			},
 
 			close(){
-				this.dialog = false
+				this.dialog = false;
 				setTimeout(() =>{
-					this.editedItem = Object.assign({}, this.defaultItem)
+					this.editedItem = Object.assign({}, this.defaultItem);
 					this.editedIndex = -1
 				}, 300)
 			},
@@ -142,20 +168,6 @@
 	}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-	h3{
-		margin: 40px 0 0;
-	}
-	ul{
-		list-style-type: none;
-		padding: 0;
-	}
-	li{
-		display: inline-block;
-		margin: 0 10px;
-	}
-	a{
-		color: #42b983;
-	}
+
 </style>
